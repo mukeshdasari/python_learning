f7a136cd0e4a378ef194ddfb2b1e5c9d7c5e7363
mukeshdasari <mukeshdasari911@gmail.com>
2020-04-07 11:18:02 +0530

    Modified the main logic and added keyboard interrupt code along with handled different application errors

--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
                         print('Selected choice is already marked. Please enter valice choice.')
                         continue
                 else:
-                    print('Please Inter choice within range 1-9')
+                    print('Please enter choice within range 1-9')
                     continue
             else:
                 print("Please enter Integer choice.")
