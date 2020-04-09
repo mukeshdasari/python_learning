ea5f1d2c7566a7679274aad2e0ee85ba1250458e
mukeshdasari <mukeshdasari911@gmail.com>
2020-04-09 12:28:04 +0530

    Added hackerrank repo with initial solutions of hackerrank problems

--- hackerrank/find_a_string.py
+++ hackerrank/find_a_string.py
@@ -9,6 +9,5 @@ def count_substring(string, sub_string):
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
-    
     count = count_substring(string, sub_string)
     print(count)
