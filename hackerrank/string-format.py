048310e92a425977f1d1c3c5e190b97049f0f477
mukeshdasari <mukeshdasari911@gmail.com>
2020-04-10 20:09:59 +0530

    added string-format solution which includes decimal to binary, hex and binary conversion using .format method

---
+++ hackerrank/string-format.py
@@ -0,0 +1,9 @@
+def print_formatted(number):
+    # your code goes here
+    space = len("{0:b}".format(number))
+    for x in range(1, number+1):
+        print("{0:{space}d} {0:{space}o} {0:{space}X} {0:{space}b}".format(x, space = space))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
